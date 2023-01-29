ee28deb417a42431ee0f1e303b1001efa66e523e
Maurane van der Stoep <mauranevds@gmail.com>
2023-01-29 22:21:41 +0000

    generate scrabble tiles and check for randomness

--- src/pij/main/Bag.java
+++ src/pij/main/Bag.java
@@ -1,6 +1,7 @@
 package pij.main;
 
 import java.util.ArrayList;
+import java.util.Collections; 
 
 public class Bag {
 
@@ -9,18 +10,43 @@ public class Bag {
 	public Bag() {
 		int numOfTiles = 100;
 		tiles = new ArrayList<Tile>(numOfTiles);
-		for(int i = 0; i < numOfTiles; i++) {
+		createTiles('A', 1, 9);
+		createTiles('B', 3, 2);
+		createTiles('C', 3, 2);
+		createTiles('D', 2, 4);
+		createTiles('E', 1, 12);
+		createTiles('F', 4, 2);
+		createTiles('G', 2, 3);
+		createTiles('H', 4, 2);
+		createTiles('I', 1, 9);
+		createTiles('J', 8, 1);
+		createTiles('K', 5, 1);
+		createTiles('L', 1, 4);
+		createTiles('M', 3, 2);
+		createTiles('N', 1, 6);
+		createTiles('O', 1, 8);
+		createTiles('P', 3, 2);
+		createTiles('Q', 10, 1);
+		createTiles('R', 1, 6);
+		createTiles('S', 1, 4);
+		createTiles('T', 1, 6);
+		createTiles('U', 1, 4);
+		createTiles('V', 4, 2);
+		createTiles('W', 4, 2);
+		createTiles('X', 8, 1);
+		createTiles('Y', 4, 2);
+		createTiles('Z', 10, 1);
+		createTiles(' ', 3, 2);
+		
+		Collections.shuffle(tiles); //what is Collections here?
+	}
+	
+	private void createTiles(char letter, int score, int multiple) {
+		for (int i = 0; i < multiple; i++) {
 			Tile tile = new Tile();
-			if (i % 2 == 0) {
-				tile.setLetter('A');
-				tile.setScore(1);
-			}
-			else {
-				tile.setLetter('B');
-				tile.setScore(3);
-			}
-
-			tiles.add(tile);
+			tile.setLetter(letter);
+			tile.setScore(score);
+			this.tiles.add(tile);
 		}
 	}
 
--- src/pij/main/TestBag.java
+++ src/pij/main/TestBag.java
@@ -1,6 +1,7 @@
 package pij.main;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
@@ -10,6 +11,8 @@ import java.util.Comparator;
 import org.junit.jupiter.api.Test ;
 
 public class TestBag {
+
+    String grumpyduck = "quackhiss!";
 
     @Test
     void testTakeTile() {
@@ -22,7 +25,7 @@ public class TestBag {
     @Test
     void testTakeTileThrowsExceptionWhenEmpty() {
         // check we get an exception on 101 goes of takeTile();
-        String expectedMessage = "There are no more tiles!";
+    	String expectedMessage = "There are no more tiles!";
 
         int numOfTiles = 100;
         Bag bag = new Bag();
@@ -41,36 +44,37 @@ public class TestBag {
         }
     }
 
+	String expectedTiles = " 3 3"
+    		+ "A1A1A1A1A1A1A1A1A1"
+            + "B3B3"
+            + "C3C3"
+            + "D2D2D2D2"
+            + "E1E1E1E1E1E1E1E1E1E1E1E1"
+            + "F4F4"
+            + "G2G2G2"
+            + "H4H4"
+            + "I1I1I1I1I1I1I1I1I1"
+            + "J8"
+            + "K5"
+            + "L1L1L1L1"
+            + "M3M3"
+            + "N1N1N1N1N1N1"
+            + "O1O1O1O1O1O1O1O1"
+            + "P3P3"
+            + "Q10"
+            + "R1R1R1R1R1R1"
+            + "S1S1S1S1"
+            + "T1T1T1T1T1T1"
+            + "U1U1U1U1"
+            + "V4V4"
+            + "W4W4"
+            + "X8"
+            + "Y4Y4"
+            + "Z10";
+
     @Test
     void testTilesAreExpectedTiles() {
         //check tiles are the expected 100 tiles
-        String expectedTiles = "A1A1A1A1A1A1A1A1A1"
-                + "B3B3"
-                + "C3C3"
-                + "D2D2D2D2"
-                + "E1E1E1E1E1E1E1E1E1E1E1E1"
-                + "F4F4"
-                + "G2G2G2"
-                + "H4H4"
-                + "I1I1I1I1I1I1I1I1I1"
-                + "J8"
-                + "K5"
-                + "L1L1L1L1"
-                + "M3M3"
-                + "N1N1N1N1N1N1"
-                + "O1O1O1O1O1O1O1O1"
-                + "P3P3"
-                + "Q10"
-                + "R1R1R1R1R1R1"
-                + "S1S1S1S1"
-                + "T1T1T1T1T1T1"
-                + "U1U1U1U1"
-                + "V4V4"
-                + "W4W4"
-                + "X8"
-                + "Y4Y4"
-                + "Z10"
-                + " 3 3";
 
         class TileComparator implements Comparator<Tile> {
             public int compare(Tile a, Tile b) {
@@ -98,5 +102,37 @@ public class TestBag {
             actualTiles = actualTiles + tile.toString();
         }
 		assertEquals(expectedTiles, actualTiles );
+    }
+    
+    @Test
+    void testRandomness() {
+    	//check tiles come out in a random order
+    	String unexpectedTile = "A1";
+    	String unexpectedTiles = "A1A1A1A1A1A1A1A1A1B3";
+    	String actual10Tiles = "";
+    	
+    	Bag bag = new Bag();
+    	Tile tile = bag.takeTile();
+    	String actualTile1 = tile.toString();
+    	System.out.println(actualTile1);
+    	assertNotEquals(unexpectedTile, actualTile1);
+    	
+    	Bag bag2 = new Bag();
+    	tile = bag2.takeTile();
+    	String actualTile2 = tile.toString();
+    	System.out.println(actualTile2);
+    	assertNotEquals(actualTile1, actualTile2);
+    	
+        ArrayList<Tile> randomtiles = new ArrayList<Tile>();
+
+    	for(int i = 0; i < 10; i++) {
+    		tile = bag.takeTile();
+    		randomtiles.add(tile);
+    		actual10Tiles = randomtiles.toString();
+    	}
+    	System.out.println(actual10Tiles);
+    	assertNotEquals(unexpectedTiles, actual10Tiles);
+    	
+    	
     }
 }
