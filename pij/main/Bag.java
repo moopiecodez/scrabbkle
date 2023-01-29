16c17b997bf2e63deaab963d5759e8db579f7b7b
Maurane van der Stoep <mauranevds@gmail.com>
2023-01-29 10:57:34 +0000

    Started testing bag creation

--- pij/main/Bag.java
+++ pij/main/Bag.java
@@ -3,17 +3,27 @@ package pij.main;
 import java.util.ArrayList;
 
 public class Bag {
-	
-	ArrayList<Tile> tiles;
-	
+
+	private ArrayList<Tile> tiles;
+
 	public Bag() {
 		int numOfTiles = 100;
 		tiles = new ArrayList<Tile>(numOfTiles);
 		for(int i = 0; i < numOfTiles; i++) {
 			Tile tile = new Tile();
+			if (i % 2 == 0) {
+				tile.setLetter('A');
+				tile.setScore(1);
+			}
+			else {
+				tile.setLetter('B');
+				tile.setScore(3);
+			}
+
 			tiles.add(tile);
 		}
 	}
+
 	public Tile takeTile() {
 		if(tiles.isEmpty()) {
 			throw new InsufficientTilesException();
@@ -21,5 +31,8 @@ public class Bag {
 		Tile tile = tiles.remove(0);
 		return tile;
 	}
-	
+
+	public boolean isEmpty() {
+		return tiles.isEmpty();
+	}
 }
--- pij/main/TestBag.java
+++ pij/main/TestBag.java
@@ -4,39 +4,99 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+
 import org.junit.jupiter.api.Test ;
 
 public class TestBag {
-	
-	@Test
-	void testTakeTile() {
-		//check method takeTile takes a tile from bag
-		Bag bag = new Bag();
-		Object something = bag.takeTile();
-		assertTrue(something instanceof Tile);
-	}
-	
-	@Test
-	void testTakeTileThrowsExceptionWhenEmpty() {
-		// check we get an exception on 101 goes of takeTile();
+
+    @Test
+    void testTakeTile() {
+        //check method takeTile takes a tile from bag
+        Bag bag = new Bag();
+        Object something = bag.takeTile();
+        assertTrue(something instanceof Tile);
+    }
+
+    @Test
+    void testTakeTileThrowsExceptionWhenEmpty() {
+        // check we get an exception on 101 goes of takeTile();
         String expectedMessage = "There are no more tiles!";
-		Bag bag = new Bag();
-		int numOfTiles = 100;
-		for(int i = 0; i < numOfTiles; i++) {
-			bag.takeTile();
-		}
-		try {
-			bag.takeTile();
-			fail("expected InsufficientTilesException");
-		}
-       catch (InsufficientTilesException exception) {
+
+        int numOfTiles = 100;
+        Bag bag = new Bag();
+
+        for(int i = 0; i < numOfTiles; i++) {
+            bag.takeTile();
+        }
+
+        try {
+            bag.takeTile();
+            fail("expected InsufficientTilesException");
+        }
+        catch (InsufficientTilesException exception) {
             String actualMessage = exception.getMessage();
             assertEquals(expectedMessage, actualMessage);
         }
-	}
-	
-	@Test
-	void testTilesAreExpectedTiles() {
-		//check tiles are the expected 100 tiles
-	}
+    }
+
+    @Test
+    void testTilesAreExpectedTiles() {
+        //check tiles are the expected 100 tiles
+        String expectedTiles = "A1A1A1A1A1A1A1A1A1"
+                + "B3B3"
+                + "C3C3"
+                + "D2D2D2D2"
+                + "E1E1E1E1E1E1E1E1E1E1E1E1"
+                + "F4F4"
+                + "G2G2G2"
+                + "H4H4"
+                + "I1I1I1I1I1I1I1I1I1"
+                + "J8"
+                + "K5"
+                + "L1L1L1L1"
+                + "M3M3"
+                + "N1N1N1N1N1N1"
+                + "O1O1O1O1O1O1O1O1"
+                + "P3P3"
+                + "Q10"
+                + "R1R1R1R1R1R1"
+                + "S1S1S1S1"
+                + "T1T1T1T1T1T1"
+                + "U1U1U1U1"
+                + "V4V4"
+                + "W4W4"
+                + "X8"
+                + "Y4Y4"
+                + "Z10"
+                + " 3 3";
+
+        class TileComparator implements Comparator<Tile> {
+            public int compare(Tile a, Tile b) {
+                char x = a.getLetter();
+                char y = b.getLetter();
+                int comparison = Character.compare(x, y);
+                return comparison;
+            }
+        }
+
+        Comparator<Tile> comparator = new TileComparator();
+        ArrayList<Tile> tiles = new ArrayList<Tile>();
+
+        Bag bag = new Bag();
+
+        while(!bag.isEmpty()) {
+            Tile tile = bag.takeTile();
+            tiles.add(tile);
+        }
+
+        tiles.sort(comparator);
+
+        String actualTiles = "";
+        for (Tile tile : tiles) {
+            actualTiles = actualTiles + tile.toString();
+        }
+		assertEquals(expectedTiles, actualTiles );
+    }
 }
