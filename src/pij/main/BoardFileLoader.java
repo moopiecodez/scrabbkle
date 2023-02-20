6531b05aa2ed0ba533d3dd8cdd5f8408ec1726be
Maurane van der Stoep <mauranevds@gmail.com>
2023-02-20 06:13:30 +0000

    refactored to use bufferedReader instead of characters

--- src/pij/main/BoardFileLoader.java
+++ src/pij/main/BoardFileLoader.java
@@ -1,31 +1,23 @@
 package pij.main;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.stream.Stream;
 
 public class BoardFileLoader {
     public static ArrayList<String> load(String pathname) throws IOException {
         ArrayList<String> lines = new ArrayList<String>();
         File file = new File(pathname);
         FileReader reader = new FileReader(file);
+        BufferedReader buff = new BufferedReader(reader);
         String line = "";
 
-        int i;
-        while ( (i = reader.read()) != -1) {
-            char c = (char) i;
-            if(c == '\n') {
-                lines.add(line);
-                line = "";
-            }
-            else {
-                line = line + c;
-            }
+        while ( (line = buff.readLine()) != null) {
+            lines.add(line);
         }
-        lines.add(line);
+        buff.close();
         return lines;
     }
 }
