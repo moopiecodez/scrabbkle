ccaf7d608b1b0315baba1c89df86bb5539a725ae
Maurane van der Stoep <mauranevds@gmail.com>
2023-03-19 21:26:25 +0000

    Refactor Board to be constructed with one string of tokens.

--- src/pij/main/Board.java
+++ src/pij/main/Board.java
@@ -1,43 +1,78 @@
 package pij.main;
 
+import java.util.Scanner;
 import pij.main.square.Square;
 
 public class Board {
 
     private Square[][] matrix;
     private int size;
-
-    public Board(int size, Square[][] squareMatrix) {
-        this.matrix = squareMatrix;
-        this.size = size; 
-    }
     public static final int MIN_BOARD_SIZE = 12;
     public static final int MAX_BOARD_SIZE = 26;
-    
+
+    public Board(int size, String squareString) {
+        if(size < Board.MIN_BOARD_SIZE || size > Board.MAX_BOARD_SIZE) {
+            String message =
+                    "Invalid board size: " + size + " not in range "
+                    + Board.MIN_BOARD_SIZE + "-" + Board.MAX_BOARD_SIZE;
+            throw new IllegalArgumentException(message);
+        }
+        this.size = size;
+        this.matrix = squaresMatrix(size, squareString);
+    }
+
+    /**
+     * Creates a matrix of squares based on the string extracted from the board
+     * file.
+     * @param size of the board
+     * @param squareString string of tokens representing squares
+     * @return a matrix of squares (standard, premiumletter and premiumword)
+     */
+    public static Square[][] squaresMatrix(int size, String squareString) {
+        Square[][] matrix = new Square[size][size];
+        Scanner scanner = new Scanner(squareString);
+        String standard = "\\.";
+        String multiplier = "(-\\d|\\d{1,2})";
+        String premiumLetter = String.format("\\(%s\\)", multiplier);
+        String premiumWord = String.format("\\{%s\\}", multiplier);
+        String regex = String.format("%s|%s|%s",
+                standard, premiumLetter, premiumWord);
+
+        for(int i = 0; i < size; i ++) {
+            for(int j = 0; j < size; j++) {
+                String token = scanner.findInLine(regex);
+                matrix[i][j] = Square.create(token);
+            }
+        }
+        scanner.close();
+
+        return matrix;
+    }
+
     /**
      * Turn a row of Squares into a String.
      *  1 {3} . (5)
      */
     private String rowToString(int rowIndex) {
-        String string = " " + (rowIndex + 1) + " ";
+        int rowNumber = rowIndex + 1;
+        String squares = "";
         Square[] row = this.matrix[rowIndex];
         for(int i = 0; i < this.size; i++) {
-            string += row[i];
+            squares += row[i];
         }
-        string += "\n";
+        String string = String.format("%2d %s\n", rowNumber, squares);
         return string;
     }
-    
+
     public String toString() {
-        
         char c;
-        String string =  "  ";
+        String string = "  ";
         for(int i = 0; i < size; i ++) {
             c = (char) ('a' + i);
             string += "  " + c;
         }
         string += " \n";
-        
+
         for(int i = 0; i < size; i++) {
             string += rowToString(i);
         }
@@ -48,7 +83,9 @@ public class Board {
     public Square getSquare(int row, char column) {
         int rowIndex = row -1;
         int columnIndex = column - 'A';
-        Square square = this.matrix[rowIndex][columnIndex] ;
+        Square square = this.matrix[rowIndex][columnIndex];
+
         return square;
     }
+
 }
--- src/pij/main/TestBoard.java
+++ src/pij/main/TestBoard.java
@@ -10,93 +10,98 @@ import pij.main.square.Square;
 import pij.main.square.StandardSquare;
 
 public class TestBoard {
+
+    @Test
+    void createSquaresMatrixFor3By3Board() {
+        int size = 3;
+        String squareString = ".........";
+
+        Square[][] expectedMatrix = new Square[size][size];
+        expectedMatrix[0][0] = new StandardSquare();
+        expectedMatrix[0][1] = new StandardSquare();
+        expectedMatrix[0][2] = new StandardSquare();
+        expectedMatrix[1][0] = new StandardSquare();
+        expectedMatrix[1][1] = new StandardSquare();
+        expectedMatrix[1][2] = new StandardSquare();
+        expectedMatrix[2][0] = new StandardSquare();
+        expectedMatrix[2][1] = new StandardSquare();
+        expectedMatrix[2][2] = new StandardSquare();
+        
+        Square[][] actualMatrix = Board.squaresMatrix(size, squareString);
+        int matrixHeight = actualMatrix.length;
+        int matrixWidth = actualMatrix[0].length;
+
+        assertEquals(size, matrixHeight);
+        assertEquals(size, matrixWidth);
+
+    }
+
     @Test
     public void squareToString() {
+        String squareString =  
+                "{3}..(2)...{3}...(2)..{3}"
+                +".{2}...(3)...(3)...{2}."
+                +"..{2}...(2).(2)...{2}.."
+                +"(2)..{2}...(2)...{2}..(2)"
+                +"....{2}.....{2}...."
+                +".(3)...(3)...(3)...(3)."
+                +"..(2)...(2).(2)...(2).."
+                +"{3}..(2)...{2}...(2)..{3}"
+                +"..(2)...(2).(2)...(2).."
+                +".(3)...(3)...(3)...(3)."
+                +"....{2}.....{2}...."
+                +"(2)..{2}...(2)...{2}..(2)"
+                +"..{2}...(2).(2)...{2}.."
+                +".{2}...(3)...(3)...{2}."
+                +"{3}..(2)...{3}...(2)..{3}";
         String expectedString = 
-                    "    a  b  c \n"
-                  + " 1 {3} . (5)\n"
-                  + " 2  . {21(-8\n"
-                  + " 3 Z10I1 P3 \n";
+                  "    a  b  c  d  e  f  g  h  i  j  k  l  m  n  o \n"
+                + " 1 {3} .  . (2) .  .  . {3} .  .  . (2) .  . {3}\n"
+                + " 2  . {2} .  .  . (3) .  .  . (3) .  .  . {2} . \n"
+                + " 3  .  . {2} .  .  . (2) . (2) .  .  . {2} .  . \n"
+                + " 4 (2) .  . {2} .  .  . (2) .  .  . {2} .  . (2)\n"
+                + " 5  .  .  .  . {2} .  .  .  .  . {2} .  .  .  . \n"
+                + " 6  . (3) .  .  . (3) .  .  . (3) .  .  . (3) . \n"
+                + " 7  .  . (2) .  .  . (2) . (2) .  .  . (2) .  . \n"
+                + " 8 {3} .  . (2) .  .  . {2} .  .  . (2) .  . {3}\n"
+                + " 9  .  . (2) .  .  . (2) . (2) .  .  . (2) .  . \n"
+                + "10  . (3) .  .  . (3) .  .  . (3) .  .  . (3) . \n"
+                + "11  .  .  .  . {2} .  .  .  .  . {2} .  .  .  . \n"
+                + "12 (2) .  . {2} .  .  . (2) .  .  . {2} .  . (2)\n"
+                + "13  .  . {2} .  .  . (2) . (2) .  .  . {2} .  . \n"
+                + "14  . {2} .  .  . (3) .  .  . (3) .  .  . {2} . \n"
+                + "15 {3} .  . (2) .  .  . {3} .  .  . (2) .  . {3}\n";
 
-        int size = 3;
-        Tile tileA = new Tile('Z', 10);
-        Tile tileB = new Tile('I', 1);
-        Tile tileC = new Tile('P', 3);
-        Square [][] squareMatrix =  new Square [size][size];
-            squareMatrix[0][0] = new PremiumWordSquare(3);
-            squareMatrix[0][1] = new StandardSquare();
-            squareMatrix[0][2] = new PremiumLetterSquare(5);
-            squareMatrix[1][0] = new StandardSquare();
-            squareMatrix[1][1] = new PremiumWordSquare(21);
-            squareMatrix[1][2] = new PremiumLetterSquare(-8);
-            squareMatrix[2][0] = new StandardSquare();
-            squareMatrix[2][1] = new PremiumWordSquare(2);
-            squareMatrix[2][2] = new PremiumLetterSquare(3);
-        
-        squareMatrix[2][0].setTile(tileA);
-        squareMatrix[2][1].setTile(tileB);
-        squareMatrix[2][2].setTile(tileC);
-
-        Board board = new Board(size, squareMatrix);
+        int size = 15;
+   
+        Board board = new Board(size, squareString);
         String actualString = board.toString();
-
+        System.out.println(actualString);
         assertEquals(expectedString, actualString);
     }
 
     @Test
     public void pickSquare() {
-        int size = 4;
-        int row = 3;
+        int size = 15;
+        int row = 2;
         char column = 'B';
 
-        Tile tileA = new Tile('Z', 10);
-        Tile tileB = new Tile('I', 1);
-        Tile tileC = new Tile('P', 3);
-        Square [][] squareMatrix =  new Square [size][size];
-            squareMatrix[0][0] = new PremiumWordSquare(3);
-            squareMatrix[0][1] = new StandardSquare();
-            squareMatrix[0][2] = new PremiumLetterSquare(5);
-            squareMatrix[0][3] = new StandardSquare();
-            squareMatrix[1][0] = new StandardSquare();
-            squareMatrix[1][1] = new PremiumWordSquare(21);
-            squareMatrix[1][2] = new PremiumLetterSquare(-8);
-            squareMatrix[1][3] = new StandardSquare();
-            squareMatrix[2][0] = new StandardSquare();
-            squareMatrix[2][1] = new PremiumWordSquare(2);
-            squareMatrix[2][2] = new PremiumLetterSquare(3);
-            squareMatrix[2][3] = new StandardSquare();
-            squareMatrix[3][0] = new PremiumWordSquare(3);
-            squareMatrix[3][1] = new StandardSquare();
-            squareMatrix[3][2] = new PremiumLetterSquare(5);
-            squareMatrix[3][3] = new StandardSquare();
-            
-        squareMatrix[2][0].setTile(tileA);
-        squareMatrix[2][1].setTile(tileB);
-        squareMatrix[2][2].setTile(tileC);
+        String squareString =
+                "{3}..(2)...{3}...(2)..{3}.{2}...(3)...(3)...{2}."
+                +"..{2}...(2).(2)...{2}..(2)..{2}...(2)...{2}..(2)"
+                +"....{2}.....{2}.....(3)...(3)...(3)...(3)."
+                +"..(2)...(2).(2)...(2)..{3}..(2)...{2}...(2)..{3}"
+                +"..(2)...(2).(2)...(2)...(3)...(3)...(3)...(3)."
+                +"....{2}.....{2}....(2)..{2}...(2)...{2}..(2)"
+                +"..{2}...(2).(2)...{2}...{2}...(3)...(3)...{2}."
+                +"{3}..(2)...{3}...(2)..{3}";
 
-        Board board = new Board(size, squareMatrix);
-        Square expectedSquare = squareMatrix[2][1];
+        Board board = new Board(size, squareString);
+        String expectedString = "{2}";
 
-        Square actualSquare = board.getSquare(row, column);
-
-        assertEquals(expectedSquare, actualSquare);
+        Square square = board.getSquare(row, column);
+        String actualString = square.toString();
+        assertEquals(expectedString, actualString);
     }
-    /* Saving for later composite test
-     *  "    a  b  c  d  e  f  g  h  i  j  k  l  m  n  o \n"
-      + " 1 {3} .  . (2) .  .  . {3} .  .  . (2) .  . {3}\n"
-      + " 2  . {2} .  .  . (3) .  .  . (3) .  .  . {2} . \n"
-      + " 3  .  . {2} .  .  . (2) . (2) .  .  . {2} .  . \n"
-      + " 4 (2) .  . {2} .  .  . (2) .  .  . {2} .  . (2)\n"
-      + " 5  .  .  .  . {2} .  .  .  .  . {2} .  .  .  . \n"
-      + " 6  . (3) .  .  . (3) .  .  . (3) .  .  . (3) . \n"
-      + " 7  .  . (2) .  .  . (2) . (2) .  .  . (2) .  . \n"
-      + " 8 {3} .  . (2) .  .  . {2} .  .  . (2) .  . {3}\n"
-      + " 9  .  . (2) .  .  . (2) . (2) .  .  . (2) .  . \n"
-      + "10  . (3) .  .  . (3) .  .  . (3) .  .  . (3) . \n"
-      + "11  .  .  .  . {2} .  .  .  .  . {2} .  .  .  . \n"
-      + "12 (2) .  . {2} .  .  . (2) .  .  . {2} .  . (2)\n"
-      + "13  .  . {2} .  .  . (2) . (2) .  .  . {2} .  . \n"
-      + "14  . {2} .  .  . (3) .  .  . (3) .  .  . {2} . \n"
-      + "15 {3} .  . (2) .  .  . {3} .  .  . (2) .  . {3}\n";
-     */ 
+
 }
