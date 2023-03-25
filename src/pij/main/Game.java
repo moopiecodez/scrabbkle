9ea145c36e32b517ffbe9ad2076943a5d0da4215
Maurane van der Stoep <mauranevds@gmail.com>
2023-03-25 21:36:00 +0000

    Continue work on game play

--- src/pij/main/Game.java
+++ src/pij/main/Game.java
@@ -8,14 +8,53 @@ package pij.main;
 public class Game {
 
     private Board board;
+    private Bag bag;
+    private Rack userRack;
+    private Rack computerRack;
+    private Player player1;
+    private Player player2;
+    private Player activePlayer;
 
-    public Game(Board board) {
+    //think can delete as in getmove human
+    //private static final String TILE_MSG = "It's your turn! Your tiles:";
+    //will need to be String.format()'d later
+    private static final String MOVE_MSG_FMT = "The move is:    Word:"
+            + " %s at position %s, direction: %s";
+    private static final String SCORE_MSG_FMT = 
+             "Human player score:       %d \n"
+            +"Computer player score:    %d \n";
+    private static final String USER_PLAY_MSG = 
+            "Please enter your move with letter sequence, position, and\n"
+            + "direction (d for down, r for right) separated by commas.\n"
+            + "Entering just two commas passes.";
+
+    public Game(
+            Board board, Bag bag, Rack userRack, Rack computerRack,
+            Player human, Player computer) {
         this.board = board;
+        this.bag = bag;
+        this.userRack = userRack;
+        this.computerRack = computerRack;
+        this.player1 = human;
+        this.player2 = computer;
+        this.activePlayer = player1;
     }
 
     public void play() {
+        //game opening
         System.out.println(this.board);
-        System.out.println("It's your turn! Your tiles:");
-        System.out.println("[T1], [I1], [U1], [M3], [G2], [R1], [L1]");
+        System.out.println(TILE_MSG);
+        //move to human  turn System.out.println(this.userRack.toString());
+        //from user
+        activePlayer.getMove();
+        //make loop until game ends
+        System.out.println(String.format(SCORE_MSG_FMT, humanScore, computerScore));
+        System.out.println(this.board);
+        //if human only
+        System.out.println(this.userRack.toString()));
+        // gets user or computer move, validates it and updates score
+        getMove();
+        System.out.println(String.format(MOVE_MSG_FMT, word, position, direction));
     }
+
 }
