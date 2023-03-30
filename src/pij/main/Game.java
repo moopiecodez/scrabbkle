ccd040057b8e0a9579cbce9325b18174e09a6a42
Maurane van der Stoep <mauranevds@gmail.com>
2023-03-30 20:48:56 +0100

    Refactor game.play(), extract display methods.

--- src/pij/main/Game.java
+++ src/pij/main/Game.java
@@ -2,6 +2,7 @@ package pij.main;
 
 /**
  * The classic word game Scrabbkle.
+
  * @author Maurane van der Stoep
  *
  */
@@ -13,22 +14,13 @@ public class Game {
     private Player player2;
     private Player activePlayer;
 
-    //think can delete as in getmove human
-    //private static final String TILE_MSG = "It's your turn! Your tiles:";
-    //will need to be String.format()'d later
-    private static final String MOVE_MSG_FMT = "The move is:    Word:"
-            + " %s at position %s, direction: %s";
+    private static final String MOVE_MSG_FMT = "The move is:\t%s";
     private static final String SCORE_MSG_FMT = 
-             "Human player score:       %d \n"
-            +"Computer player score:    %d \n";
-    private static final String USER_PLAY_MSG = 
-            "Please enter your move with letter sequence, position, and\n"
-            + "direction (d for down, r for right) separated by commas.\n"
-            + "Entering just two commas passes.";
+              "Human player score:       %d \n"
+            + "Computer player score:    %d \n";
 
     public Game(
-            Board board, Bag bag,
-            Player human, Player computer) {
+            Board board, Bag bag, Player human, Player computer) {
         this.board = board;
         this.bag = bag;
         this.player1 = human;
@@ -37,20 +29,34 @@ public class Game {
     }
 
     public void play() {
+        Move move;
         //game opening
-        System.out.println(this.board);
-        //System.out.println(TILE_MSG);
-        //move to human  turn System.out.println(this.userRack.toString());
-        //from user
-        activePlayer.getMove();
-        //make loop until game ends
+        displayBoard(this.board);
+        move = activePlayer.chooseMove(this.board);
+        displayMove(move);
+        activePlayer.playMove(this.board, move);
+        displayBoard(this.board);
+        }
+        //make loop until game ends, switch active player, print score and board
        // System.out.println(String.format(SCORE_MSG_FMT, humanScore, computerScore));
         //System.out.println(this.board);
         //if human only
-        //System.out.println(this.userRack.toString()));
         // gets user or computer move, validates it and updates score
         //getMove();
-        //System.out.println(String.format(MOVE_MSG_FMT, word, position, direction));
+    //}
+
+    /** Displays the current board. */
+    public static void displayBoard(final Board board) {
+        System.out.println(board);
     }
 
+    /** Displays the current move. */
+    public static void displayMove(final Move move) {
+        String msg = String.format(MOVE_MSG_FMT, move);
+        System.out.println(msg);
+    }
+
+    public static void displayScore() {
+        //TODO implement displayscore method
+    }
 }
